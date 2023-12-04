7b71dbdce0aee351f08d118e048560a87ad394ac
Gustavo Paulino <cast.gustavo.utic@sebrae.com.br>
2023-12-04 16:18:02 -0300

    adicionando estrutura inicial

---
+++ src/partials/Header/Header.style.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+// JSS - Java Script Stylesheet - makestyle com uma arrow function com retorno implícito
+const useStyles = makeStyles(() => ({
+    title: {
+        flexGrow: 1,
+    }
+}))
+
+export default useStyles
